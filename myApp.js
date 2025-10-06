3385740ae3374a1f9886f8fd6df550dadc24a412
MihailBita <mihail.bita@gmail.com>
2025-10-06 17:14:59 +0300

    renaming app

--- myApp.js
+++ myApp.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
 const app = express();
-const port = 3000;
 
 app.get('/', (req, res) => {
   res.json({ message: `Hello World!` });
